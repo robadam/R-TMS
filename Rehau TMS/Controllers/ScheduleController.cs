48f4866b8ad738444dd3538684c2713355dc43e1
robadam <35897668+robadam@users.noreply.github.com>
2018-06-17 09:09:40 +0200

    Add OWIN, Identity packages

--- Rehau TMS/Controllers/ScheduleController.cs
+++ Rehau TMS/Controllers/ScheduleController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,11 @@ namespace Rehau_TMS.Controllers
     public class ScheduleController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
         {
             return View();
         }
