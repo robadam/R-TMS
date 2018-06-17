beef6051eade408c1b16c37b3670af42c1ff1052
robadam <35897668+robadam@users.noreply.github.com>
2018-06-17 16:39:57 +0200

    Initial migration

---
+++ Rehau TMS/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Rehau_TMS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rehau_TMS.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Rehau_TMS.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
