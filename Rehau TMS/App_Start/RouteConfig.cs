8cef1899d529c6e6e332cfabab553c3c75a48519
robadam <35897668+robadam@users.noreply.github.com>
2018-06-17 08:38:07 +0200

    Initial

--- Rehau TMS/App_Start/RouteConfig.cs
+++ Rehau TMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Rehau_TMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Schedule", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Rehau TMS/Controllers/ScheduleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rehau_TMS.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
