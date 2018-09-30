13de1244b5393196ec5bf321e61114952d977abc
robadam <35897668+robadam@users.noreply.github.com>
2018-09-30 16:36:01 +0200

    IntialForReports
    IntialForReports

---
+++ Rehau TMS/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rehau_TMS.ViewModels;
+using Rehau_TMS.Models;
+
+namespace Rehau_TMS.Controllers
+{
+    public class ReportController : Controller
+    {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
